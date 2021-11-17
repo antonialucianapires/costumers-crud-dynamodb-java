7a2cf810678cbe458f986f02abc7430113e51849
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-11-17 01:04:02 -0300

    config: criação da tabela no dynamodb + docker-compose

---
+++ src/main/java/com/alps/consumers/CostumersApplication.java
@@ -0,0 +1,13 @@
+package com.alps.consumers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostumersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alps/consumers/CostumersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alps.consumers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostumersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
